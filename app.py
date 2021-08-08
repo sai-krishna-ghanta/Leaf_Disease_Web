ea0d4980d0eac1cd5b2de98557e773d2be5db23d
Your Name <skghanta311002@gmail.com>
2021-08-08 12:19:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,11 @@ def lab():
     return image_data,labels
 
 def app():
-
+    images = ["https://hmp.me/dovt","https://hmp.me/dovs","https://hmp.me/dovu","https://hmp.me/dovu"]
+    st.image(
+        images,
+        width=150, # Manually Adjust the width of the image as per requirement
+        )
     html_temp = """
     <body style="background-color:red;">
     <div style="background-color:Teal ;padding:10px">
@@ -96,5 +100,20 @@ def app():
         st.text(b)
         st.text(c)
         st.text(d)
+
+
+
+    st.text("This is a minor service added to the farm with app")
+    st.success("Only disease detection available in website")
+    st.text("For more services contact us")
+    if st.button("Architecture"):
+        st.success("This was made by SVM(sklearn),Opencv-Python,Streamlit and Heroku")
+        st.text("The service consits additionlly YOLOV3 leaf detector and Disease detection with openCV")
+    if st.button("Github Repository"):
+        link1 = '[GithubRepo](https://github.com/sai-krishna-ghanta/Leaf_disease_Detection)'
+        st.markdown(link1, unsafe_allow_html=True)
+    if st.button("About Developer and Contact Us"):
+        link2 = '[Linkedin Profile](https://www.linkedin.com/in/ghanta-sai-krishna-320ab0211/)'
+        st.markdown(link2, unsafe_allow_html=True)
 
 app()
